db7f79e9a14765a6c4817fdb9af5db97fa051ec9
nightdevil00 <sandorhazimihai@gmail.com>
2025-10-06 00:04:49 +0300

    Correct and improve install.sh script

---
+++ push_to_git.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Start the ssh-agent in the background
+eval "$(ssh-agent -s)"
+
+# Add your SSH key to the ssh-agent
+# You will be prompted for your passphrase here
+ssh-add /home/mihai/.ssh/id_ed25519
+
+# Push your changes to GitHub
+git push -u origin main
